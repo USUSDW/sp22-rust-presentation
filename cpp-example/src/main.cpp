a99bd136300b16c326a70c3ed03d46c72259ebab
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2022-03-19 12:43:46 -0600

    simplify cpp example

--- cpp-example/src/main.cpp
+++ cpp-example/src/main.cpp
@@ -10,16 +10,18 @@ struct NamedPointer {
         name(name), pointer(pointer) {}
 };
 
-void intPointerDemo();
-
 template <typename T>
 void printPointer(const NamedPointer<T> namedPointer);
 
-void printSeparator();
-
 int main() {
-    intPointerDemo();
-    printSeparator();
+    const auto foo = NamedPointer<int>(std::string("foo"), new int(22));
+    printPointer(foo);
+
+    *foo.pointer = 87;
+    printPointer(foo);
+
+    delete foo.pointer;
+    printPointer(foo);
 
     return 0;
 }
@@ -35,17 +37,8 @@ void intPointerDemo() {
     printPointer(foo);
 }
 
-struct MyStruct {
-    int firstMember;
-    int secondMember;
-};
-
 template <typename T>
 void printPointer(const NamedPointer<T> namedPointer) {
     std::cout << namedPointer.name << " (pointer): " << namedPointer.pointer  << std::endl;
     std::cout << namedPointer.name << " (value)  : " << *namedPointer.pointer << std::endl;
-}
-
-void printSeparator() {
-    std::cout << "----------------" << std::endl;
 }
